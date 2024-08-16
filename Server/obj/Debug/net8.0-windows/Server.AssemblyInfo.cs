eddc2cbd4bea3020a390b65c00bc3e66734fae45
HoTien2004 <hotien804@gmail.com>
2024-08-16 21:16:06 +0700

    chinh sua giao dien client/server, them thanh status

--- Server/obj/Debug/net8.0-windows/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net8.0-windows/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a754980755eb526db0e3f608ff2a53bad9df4dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4ffd39a65a781bb4bcee6e4c281cd9e91fa4dc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
